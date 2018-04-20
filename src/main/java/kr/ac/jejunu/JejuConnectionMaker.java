e56807cc77648c2c76d532bcff2f61d99afe5b2e
jiwon <llkjop24@gmail.com>
2018-04-20 15:51:14 +0900

    3. 사용자정보 관리 요구사항이 나왔습니다. Connection을 사용자정보에도 사용할 수 있도록 추상화하시오.

--- src/main/java/kr/ac/jejunu/JejuConnectionMaker.java
+++ src/main/java/kr/ac/jejunu/JejuConnectionMaker.java
@@ -1,12 +1,13 @@
 package kr.ac.jejunu;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class HallaProductDao extends ProductDao{
+public class JejuConnectionMaker implements ConnectionMaker {
     @Override
-    Connection getConnection() throws ClassNotFoundException, SQLException {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
         return DriverManager.getConnection("jdbc:mysql://localhost/jeju?characterEncoding=utf8", "jeju", "jejupw");
     }
--- src/test/java/kr/ac/jejunu/ProductDaoTest.java
+++ src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -10,11 +10,9 @@ import static org.junit.Assert.assertEquals;
 
 public class ProductDaoTest {
     private ProductDao productDao;
-    private ProductDao hallaProductDao;
     @Before
     public void setup() {
-        productDao = new JejuProductDao();
-        hallaProductDao = new HallaProductDao();
+        productDao = new ProductDao();
     }
 
     @Test
@@ -43,29 +41,29 @@ public class ProductDaoTest {
         assertEquals(product.getPrice(),insertedProduct.getPrice());
     }
 
-    @Test
-    public void hallaget() throws SQLException, ClassNotFoundException {
-        Long id = 1L;
-        String title = "제주감귤";
-        Integer price = 15000;
-
-        Product product = hallaProductDao.get(id);
-        assertEquals(id, product.getId());
-        assertEquals(title, product.getTitle());
-        assertEquals(price, product.getPrice());
-    }
-
-    @Test
-    public void hallaadd() throws SQLException, ClassNotFoundException {
-        Product product = new Product();
-
-        product.setTitle("한라봉");
-        product.setPrice(20000);
-        Long id = hallaProductDao.insert(product);
-
-        Product insertedProduct = hallaProductDao.get(id);
-        assertEquals(id, insertedProduct.getId());
-        assertEquals(product.getTitle(),insertedProduct.getTitle());
-        assertEquals(product.getPrice(),insertedProduct.getPrice());
-    }
+//    @Test
+//    public void hallaget() throws SQLException, ClassNotFoundException {
+//        Long id = 1L;
+//        String title = "제주감귤";
+//        Integer price = 15000;
+//
+//        Product product = hallaProductDao.get(id);
+//        assertEquals(id, product.getId());
+//        assertEquals(title, product.getTitle());
+//        assertEquals(price, product.getPrice());
+//    }
+//
+//    @Test
+//    public void hallaadd() throws SQLException, ClassNotFoundException {
+//        Product product = new Product();
+//
+//        product.setTitle("한라봉");
+//        product.setPrice(20000);
+//        Long id = hallaProductDao.insert(product);
+//
+//        Product insertedProduct = hallaProductDao.get(id);
+//        assertEquals(id, insertedProduct.getId());
+//        assertEquals(product.getTitle(),insertedProduct.getTitle());
+//        assertEquals(product.getPrice(),insertedProduct.getPrice());
+//    }
 }
